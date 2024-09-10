9ad40396010578c0a645b8588d00f4b376e6cdba
thecatblake <k222ryousuke@gmail.com>
2024-09-10 15:39:31 +0900

    add love icon animation

--- src/sections/posts/post-view.tsx
+++ src/sections/posts/post-view.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { Post } from '@/interfaces/posts';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { motion } from "framer-motion";
 
 type Props = {
   post: Post;
@@ -11,13 +12,42 @@ type Props = {
 
 export default function PostView({ post }: Props) {
   const [love, setLove] = useState<boolean>(false);
+
   if (!post) {
     return <div>Post not found</div>;
   }
 
   const month = post.uploadedAt.toLocaleString('default', { month: 'long' });
   const day = post.uploadedAt.getDate();
-  const loveIcon = love ? <FavoriteIcon onClick={() => setLove(!love)} /> : <FavoriteBorderIcon onClick={() => setLove(!love)} />
+
+  const loveIcon = love ? 
+    <motion.div animate={
+    { 
+      scale: [
+        1.5
+      ],
+      rotateZ: [
+        360,
+        0
+      ],
+      transition: {duration: 0.3}
+    }}>
+        <FavoriteIcon sx={{color: "pink"}} onClick={() => setLove(!love)} /> 
+    </motion.div> : 
+    <motion.div animate={
+    { 
+      scale: [
+        1.3,
+        1
+      ],
+      rotateZ: [
+        0,
+        360
+      ],
+      transition: {duration: 0.3}
+    }}>
+      <FavoriteBorderIcon sx={{color: "pink"}} onClick={() => setLove(!love)} />
+    </motion.div>
 
   return (
     <div className='container mx-auto p-8'>
